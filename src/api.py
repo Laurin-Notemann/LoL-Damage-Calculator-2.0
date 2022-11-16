0e35daf13b8ab9e4d40a65852ae95a0fddc29029
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-11-16 14:19:44 +0100

    added ItemData as API resource

--- src/api.py
+++ src/api.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource
-from jsonpickle import encode, decode 
+from jsonpickle import encode, decode
 from python_champions.A import Aatrox, Ahri, Akali
 from python_champions.S import Seraphine
 from python_items.L import LiandrysAnguish, LudensTempest
@@ -8,31 +8,33 @@ from get_dict import get_json_champ, get_json_item
 
 
 # Initialise ALL available champions + ENCODING (decoding)
-aatrox = decode(encode(Aatrox(get_json_champ("Aatrox"), 1000), unpicklable=False))
+aatrox = decode(
+    encode(Aatrox(get_json_champ("Aatrox"), 1000), unpicklable=False))
 ahri = decode(encode(Ahri(get_json_champ("Ahri")), unpicklable=False))
 akali = decode(encode(Akali(get_json_champ("Akali"), 1000), unpicklable=False))
-seraphine = decode(encode(Seraphine(get_json_champ("Seraphine"), 1000), unpicklable=False))
+seraphine = decode(
+    encode(Seraphine(get_json_champ("Seraphine"), 1000), unpicklable=False))
 
 # Initialise ALL available items + ENCDODING (decoding)
 liandrys = decode(encode(LiandrysAnguish(get_json_item("6653"))))
-ludents = decode(encode(LudensTempest(get_json_item("6655"))))
-
+ludens = decode(encode(LudensTempest(get_json_item("6655"))))
 
 app = Flask(__name__)
 api = Api(app)
 
-test = {"champion_id": 1}
 
-class DamageData(Resource):
+class ChampionData(Resource):
     def get(self):
         return {"listOfChampions": [aatrox, ahri, akali, seraphine]}
-    
 
 
+class ItemData(Resource):
+    def get(self):
+        return {"listOfItems": [liandrys, ludens]}
 
 
-api.add_resource(DamageData, "/data")
+api.add_resource(ChampionData, "/getInitChampions")
+api.add_resource(ItemData, "/getInitItems")
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
