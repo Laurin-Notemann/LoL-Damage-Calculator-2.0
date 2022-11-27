b417e766f9d95b8629c761eac875e2e3d58f3315
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-11-27 23:14:45 +0100

    fix: scaling_values are properly assigned now, removed unused methods

--- src/Champion/Champion.py
+++ src/Champion/Champion.py
@@ -133,11 +133,11 @@ class Champion:
         self.ability_q: list[Ability] = get_abilities_data(
             "Q", champ_dict, self.q_bounds)
         self.ability_w: list[Ability] = get_abilities_data(
-            "W", champ_dict, self.q_bounds)
+            "W", champ_dict, self.w_bounds)
         self.ability_e: list[Ability] = get_abilities_data(
-            "E", champ_dict, self.q_bounds)
+            "E", champ_dict, self.e_bounds)
         self.ability_r: list[Ability] = get_abilities_data(
-            "R", champ_dict, self.q_bounds)
+            "R", champ_dict, self.r_bounds)
 
         self.add_scaling_stats_values_to_ability()
 
@@ -193,11 +193,11 @@ class Champion:
             self.attack_speed_per_level / 100, 4)
 
     def set_scaling_values(self):
-        self.scaling_stats_values = [
-            ScalingValue("AD", self.total_attack_damage),
-            ScalingValue("BONUS_AD", self.bonus_attack_damage),
-            ScalingValue("AP", self.total_ability_power_flat)
-        ]
+        self.scaling_stats_values = {
+            "AD": ScalingValue("AD", self.total_attack_damage),
+            "BONUS_AD": ScalingValue("BONUS_AD", self.bonus_attack_damage),
+            "AP": ScalingValue("AP", self.total_ability_power_flat)
+        }
 
     def set_total_value_to_based_on_level_and_item_stats(self):
         # First we add all the stats from the items that are chosen
@@ -209,6 +209,7 @@ class Champion:
         self.set_base_stats_based_on_level()
         self.set_total_stats()
         self.set_scaling_values()
+        self.add_scaling_stats_values_to_ability()
 
     def auto_attack(self):
         damage_type = "PHYSICAL_DAMAGE"
@@ -235,74 +236,6 @@ class Champion:
         if ability.bounds.lower <= skill_level < ability.bounds.upper:
             return True
         return False
-
-    def get_missing_health(self, enemy_max_hp, enemy_current_hp):
-        return round((enemy_max_hp - enemy_current_hp) / enemy_max_hp, 6)
-
-    def get_amp_based_on_missing_health(self, enemy_missing_health_perc, missing_health_iterator_static, damage_amplifier_missing_health_static,
-                                        missing_health_cap):
-        missing_health_iterator = missing_health_iterator_static
-        damage_amplifier_missing_health = damage_amplifier_missing_health_static
-        for i in range(0, int((missing_health_cap / missing_health_iterator)) + 1):
-            if enemy_missing_health_perc < missing_health_iterator_static:
-                damage_amplifier_missing_health = 0
-                break
-            elif missing_health_iterator < enemy_missing_health_perc < (
-                    missing_health_iterator + missing_health_iterator_static) or enemy_missing_health_perc == missing_health_iterator:
-                break
-            missing_health_iterator = round(
-                missing_health_iterator + missing_health_iterator_static, 5)
-            damage_amplifier_missing_health = round(
-                damage_amplifier_missing_health + damage_amplifier_missing_health_static, 5)
-
-        return damage_amplifier_missing_health
-
-    def get_dmg_based_on_flat_and_percentage_values(self, key, skill_level, effect_number, attribute_number, scaling_param: str,
-                                                    scaling_param_two: str = ""):
-        scaling_value_one = 0
-        scaling_value_two = 0
-        if scaling_param == "AD":
-            scaling_value_one = self.total_attack_damage
-        elif scaling_param == "BONUS_AD":
-            scaling_value_one = self.bonus_attack_damage
-        elif scaling_param == "AP":
-            scaling_value_one = self.total_ability_power_flat
-
-        if scaling_param_two:
-            if scaling_param_two == "AD":
-                scaling_value_two = self.total_attack_damage
-            elif scaling_param_two == "BONUS_AD":
-                scaling_value_two = self.bonus_attack_damage
-            elif scaling_param_two == "AP":
-                scaling_value_two = self.total_ability_power_flat
-
-        if skill_level > -1:
-            ability_value_dict = self.get_ability_values(key)[key][0]
-
-            damage_type = ability_value_dict["damage_type"]
-            damage_flat = ability_value_dict[f"effect{effect_number}"][f"attribute{attribute_number}"][0][0]
-            damage_scaling_one = []
-            damage_scaling_two = []
-            for i in ability_value_dict[f"effect{effect_number}"][f"attribute{attribute_number}"][1][0]:
-                damage_scaling_one.append((i / 100) * scaling_value_one)
-
-            if scaling_param_two:
-                for i in ability_value_dict[f"effect{effect_number}"][f"attribute{attribute_number}"][2][0]:
-                    damage_scaling_two.append((i / 100) * scaling_value_two)
-            if scaling_param_two:
-                return [damage_flat[skill_level] + damage_scaling_one[skill_level] + damage_scaling_two[skill_level], damage_type]
-            return [damage_flat[skill_level] + damage_scaling_one[skill_level], damage_type]
-
-    def wrapper_for_dmg(self, key, skill_level, effect_number, attribute_number, scaling_param: str, scaling_param_two: str = ""):
-        """
-        This function is just a simple wrapper for clarity when reading the code, which also rounds the dmg number.
-        Third Parameter in the return call is None because when to make the array that contains the damage numbers consistent, reason being if the
-        dmg is mixed (e.g. Physical and Magical) then you get three parameters, first one states the type of dmg, and the other to each instance of
-        the mixed dmg.
-        """
-        damage_total_without_amp = self.get_dmg_based_on_flat_and_percentage_values(key, skill_level, effect_number, attribute_number,
-                                                                                    scaling_param, scaling_param_two)
-        return [damage_total_without_amp[1], round(damage_total_without_amp[0], 3), None]
 
     def add_item_stats(self):
         for i in range(1, 7):
@@ -434,9 +367,6 @@ class Champion:
         self.total_slow_resistance = round(1 - self.total_slow_resistance, 4)
 
         self.total_critical_chance = round(self.total_critical_chance, 4)
-
-        self.scaling_stats_values = [
-            self.total_attack_damage, self.bonus_attack_damage, self.total_ability_power_flat]
 
     def __getstate__(self):
         state = self.__dict__.copy()
