c44bfdef201b630db0873f56561dea1965c9bf03
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-11-27 18:25:08 +0100

    refactor: add type hints to Aatrox Class

--- src/python_champions/Aatrox.py
+++ src/python_champions/Aatrox.py
@@ -31,31 +31,31 @@ class Aatrox(Champion):
                 damage.set_damage(perc_amp*self.enemy_health)
                 return damage
 
-    def q_action(self, skill_level=-1, is_sweet_spot=False, time_casted=0):
-        q = self.ability_q[0]
+    def q_action(self, skill_level: int=-1, is_sweet_spot: bool=False, time_casted: int=0):
+        q: Ability = self.ability_q[0]
         if self.skill_level_inside_bounds(skill_level, q):
-            # Aatrox has three different cast, that all deal different amount of damage the loop goes through all three
+            # Aatrox has three different cast, that all deal different amount of damage the loop finds which cast was input
             for i in range(0, 3):
                 if i == time_casted:
                     if is_sweet_spot:
                         return q.get_damage(skill_level, i + 2, 1)
                     return q.get_damage(skill_level, i + 2, 0)
 
-    def w_action(self, skill_level=-1, tether_completed=True):
-        w = self.ability_w[0]
+    def w_action(self, skill_level: int=-1, tether_completed: bool=True):
+        w: Ability = self.ability_w[0]
         if self.skill_level_inside_bounds(skill_level, w):
             if tether_completed:
                 return w.get_damage(skill_level, 2)
             return w.get_damage(skill_level, 0)
 
-    def e_action(self, skill_level=-1):
+    def e_action(self, skill_level: int=-1):
         return None
 
-    def r_action(self, skill_level=-1):
-        r = self.ability_r[0]
+    def r_action(self, skill_level: int=-1):
+        r: Ability = self.ability_r[0]
         if self.skill_level_inside_bounds(skill_level, r):
-            r_ad = r.effects[0].attributes[0][0].values[skill_level]
-            bonus_ad_from_r = self.total_attack_damage * (1 + (r_ad / 100))
+            r_ad: float = r.effects[0].attributes[0][0].values[skill_level]
+            bonus_ad_from_r: float = self.total_attack_damage * (1 + (r_ad / 100))
             self.bonus_attack_damage += bonus_ad_from_r
             # needs to be called everytime bonus AD changes
             self.set_total_value_to_based_on_level_and_item_stats()
